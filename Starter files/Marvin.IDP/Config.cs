29a2688a0c5ebc115a35f0f18f3ad4d3de8e5978
Igor Meszaros <igor.mesaros@bighand.com>
2023-12-27 15:53:36 +0000

    Create openid client

--- Starter files/Marvin.IDP/Config.cs
+++ Starter files/Marvin.IDP/Config.cs
@@ -1,4 +1,5 @@
-﻿using Duende.IdentityServer.Models;
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
 
 namespace Marvin.IDP;
 
@@ -17,5 +18,24 @@ public static class Config
 
     public static IEnumerable<Client> Clients =>
         new Client[] 
-            { };
+            {
+                new Client()
+                {
+                    ClientName = "Image Gallery",
+                    ClientId = "imagegalleryclient",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris =
+                    {
+                        "https://localhost:7075/signin-oidc"
+                    },
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId, IdentityServerConstants.StandardScopes.Profile
+                    },
+                    ClientSecrets = 
+                    {
+                        new Secret("secret".Sha256())
+                    }
+                }
+            };
 }
