490f739a85c98a3ee72d4e8816679b878c504bda
Igor Meszaros <igor.mesaros@bighand.com>
2023-12-27 16:20:39 +0000

    Website now logs in if not logged in before

--- Starter files/Marvin.IDP/Config.cs
+++ Starter files/Marvin.IDP/Config.cs
@@ -26,7 +26,7 @@ public static class Config
                     AllowedGrantTypes = GrantTypes.Code,
                     RedirectUris =
                     {
-                        "https://localhost:7075/signin-oidc"
+                        "https://localhost:7184/signin-oidc"
                     },
                     AllowedScopes =
                     {
@@ -35,7 +35,8 @@ public static class Config
                     ClientSecrets = 
                     {
                         new Secret("secret".Sha256())
-                    }
+                    },
+                    RequireConsent = true
                 }
             };
 }
